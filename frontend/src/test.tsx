f899a51fa61950b68d6bf3a3cad7a48de7e12360
SLey3 <ghub4127@gmail.com>
2025-02-06 14:28:03 -0800

    complete sentry integration

--- frontend/src/test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TestSentry: React.FC = () => {
-    return <button onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>;
-};
-
-export default TestSentry;
